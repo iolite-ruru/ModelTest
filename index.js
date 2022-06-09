50309c68696267e75afd364fcaec05dea7f0972d
Kim So Yeon <s2023@e-mirim.hs.kr>
2022-06-10 01:20:26 +0900

    사진 업로드에 따라 결과 정상 출력

--- index.js
+++ index.js
@@ -1,58 +1,48 @@
-var submit = document.getElementById('submitButton');
-// submit.onclick = showImage;     //Submit 버튼 클릭시 이미지 보여주기
+// "use strict";
 
-// function showImage() {
-//     var newImage = document.getElementById('image-show').lastElementChild;
-//     newImage.style.visibility = "visible";
-    
-//     document.getElementById('image-upload').style.visibility = 'hidden';
+const Url = "./my_model/";
+const modelURL = Url + "model.json";
+const metadataURL = Url + "metadata.json";
 
-//     document.getElementById('fileName').textContent = null;     //기존 파일 이름 지우기
-// }
+let model;
+let file;
+let image = document.getElementById("image");
 
-var file;
 function loadFile(input) {
     file = input.files[0];
 
-    console.log(input);
-    console.log(input.files[0]);
-    console.log(file.name);
+    console.log(input.files[0].name);
+    //console.log(input);
+    // console.log(input.files[0]);
+    //console.log(file.name);
 
     var name = document.getElementById('fileName');
     name.textContent = file.name;
 
-};
-
-
-
-const Url = "./my_model/";
-
-let model, webcam, labelContainer, maxPredictions;
-
-async function init() {
-    console.log("init() call");
-    const modelURL = Url + "model.json";
-    const metadataURL = Url + "metadata.json";
-
-    console.log(modelURL);
-    console.log(metadataURL);
-
-    // load the model and metadata
-    // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
-    // or files from your local hard drive
-    // Note: the pose library adds "tmImage" object to your window (window.tmImage)
-    model = await tmImage.load(modelURL, metadataURL);
-    maxPredictions = model.getTotalClasses();
-
-    let image = document.getElementById("image");
     image.src = URL.createObjectURL(file);
     image.style.width = "500px";
     image.style.height = "500px";
-    predict(image);
+};
+
+
+async function init() {
+    console.log("init() call");
+
+
+    // console.log(modelURL);
+    // console.log(metadataURL);
+
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPredictions = model.getTotalClasses();
+
+
+
+    predict();
 }
 
-async function predict(image) {
-    const prediction = await model.predict(image);
+async function predict() {
+    console.log(file.name);
+    var prediction = await model.predict(image, false);
     console.log(prediction[0].probability);
     if(prediction[0].probability > 0.5){
         console.log('고양이');
@@ -60,8 +50,8 @@ async function predict(image) {
         console.log('그 외');
     }
 
-    for (let i = 0; i < maxPredictions; i++) {
-        const classPrediction =
-            prediction[i].className + ": " + prediction[i].probability.toFixed(2);
-    }
+    // for (let i = 0; i < maxPredictions; i++) {
+    //     const classPrediction =
+    //         prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+    // }
 }
